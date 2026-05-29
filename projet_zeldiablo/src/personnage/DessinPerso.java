24155cfbe03c04299dd66961abeca96459c8bf23
Le Madec Jean <jean.le-madec3@etu.univ-lorraine.fr>
2026-05-29 11:02:24 +0200

    update : dessinPerso

--- projet_zeldiablo/src/personnage/DessinPerso.java
+++ projet_zeldiablo/src/personnage/DessinPerso.java
@@ -5,8 +5,5 @@ import moteurJeu.DessinJeu;
 import java.awt.image.BufferedImage;
 
 public class DessinPerso implements DessinJeu {
-    @Override
-    public void dessiner(BufferedImage image) {
 
-    }
 }
