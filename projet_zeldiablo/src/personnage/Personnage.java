70791869b15e2727bd450e7eb3f68d9d9266de26
Lucas_M54 <lucasm54800@gmail.com>
2026-05-29 10:35:21 +0200

    add: Personnage

--- projet_zeldiablo/src/personnage/Personnage.java
+++ projet_zeldiablo/src/personnage/Personnage.java
@@ -4,6 +4,16 @@ import moteurJeu.Commande;
 import moteurJeu.Jeu;
 
 public class Personnage implements Jeu {
+    private int x;
+    private int y;
+
+    public void deplacer(Commande commandeUser) {
+        if (commandeUser.droite) x++;
+        if (commandeUser.gauche) x--;
+        if (commandeUser.bas) y++;
+        if (commandeUser.haut) y--;
+    }
+
     @Override
     public void evoluer(Commande commandeUser) {
 
