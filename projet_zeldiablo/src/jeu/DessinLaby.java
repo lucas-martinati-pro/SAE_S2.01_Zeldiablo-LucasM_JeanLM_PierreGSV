1fb7b1462064b0e841730eaf946f24e6f3308d4f
Lucas_M54 <lucasm54800@gmail.com>
2026-05-29 11:47:43 +0200

    add: DessinLaby methode

--- projet_zeldiablo/src/jeu/DessinLaby.java
+++ projet_zeldiablo/src/jeu/DessinLaby.java
@@ -5,8 +5,19 @@ import moteurJeu.DessinJeu;
 import java.awt.image.BufferedImage;
 
 public class DessinLaby implements DessinJeu {
+    private Jeu jeu;
+
+    public DessinLaby(Jeu jeu) {
+        if (jeu != null) this.jeu = jeu;
+        else this.jeu = new Jeu();
+    }
+
     @Override
     public void dessiner(BufferedImage image) {
+        String game = jeu.jeuToString();
 
+        for (char c : game.toCharArray()) {
+
+        }
     }
 }
