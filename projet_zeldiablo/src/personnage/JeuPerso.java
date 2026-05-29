bb21e0d11d15e60dfebd7382b46d0f87d13bf373
Lucas_M54 <lucasm54800@gmail.com>
2026-05-29 10:38:05 +0200

    add: JeuPerso

--- projet_zeldiablo/src/personnage/JeuPerso.java
+++ projet_zeldiablo/src/personnage/JeuPerso.java
@@ -1,4 +1,12 @@
 package personnage;
 
-public class JeuPerso {
+import moteurJeu.Jeu;
+
+public class JeuPerso implements Jeu {
+    private Personnage perso;
+
+    public JeuPerso(Personnage perso) {
+        if (perso != null) this.perso = perso;
+        else this.perso = new Personnage(0, 0);
+    }
 }
--- projet_zeldiablo/src/personnage/Personnage.java
+++ projet_zeldiablo/src/personnage/Personnage.java
@@ -1,26 +1,22 @@
 package personnage;
 
 import moteurJeu.Commande;
-import moteurJeu.Jeu;
 
-public class Personnage implements Jeu {
+public class Personnage {
     private int x;
     private int y;
+
+    public Personnage(int x, int y) {
+        if (x < 0) this.x = 0;
+        else this.x = x;
+        if (y < 0) this.y = 0;
+        else this.y = y;
+    }
 
     public void deplacer(Commande commandeUser) {
         if (commandeUser.droite) x++;
         if (commandeUser.gauche) x--;
         if (commandeUser.bas) y++;
         if (commandeUser.haut) y--;
-    }
-
-    @Override
-    public void evoluer(Commande commandeUser) {
-
-    }
-
-    @Override
-    public boolean etreFini() {
-        return false;
     }
 }
