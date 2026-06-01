f6d08c571a1018901512fe2d50c405067088cdab
Lucas_M54 <lucasm54800@gmail.com>
2026-06-01 10:44:57 +0200

    update: etreFini dans Jeu

--- projet_zeldiablo/src/jeu/Jeu.java
+++ projet_zeldiablo/src/jeu/Jeu.java
@@ -250,7 +250,15 @@ public class Jeu implements moteurJeu.Jeu {
      * @return true si le hero est sur la case de fin, false sinon
      */
     public boolean etreFini() {
-        return (this.hero.getX() == this.fin[0] && this.hero.getY() == this.fin[1]) || (this.hero.etreMort());
+        if (this.hero.getX() == this.fin[0] && this.hero.getY() == this.fin[1]) {
+            System.out.println("Félicitation, vous avez gagné !!!");
+            return true;
+        } else if (this.hero.etreMort()) {
+            System.out.println("Vous êtes mort ! Vous avez perdu !");
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // ==================== Compatibilités Testes ====================
