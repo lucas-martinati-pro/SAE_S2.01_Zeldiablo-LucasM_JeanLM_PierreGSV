ef8954d2a48502d08da479452bd4db01ef0770e6
Lucas_M54 <lucasm54800@gmail.com>
2026-06-03 19:42:19 +0200

    edit: lvl 1 to 5

--- projet_zeldiablo/src/zeldiablo/Jeu.java
+++ projet_zeldiablo/src/zeldiablo/Jeu.java
@@ -164,7 +164,7 @@ public class Jeu implements moteurJeu.Jeu {
                     case Jeu.VIDE -> {}
                     case Jeu.MUR -> this.cases.add(new Mur(j, i));
                     case Jeu.HERO -> {
-                        hero = new Aventurier(j, i, 50);
+                        hero = new Aventurier(j, i, 5);
                         hero.setJeu(this);
                     }
                     case Jeu.FIN -> fin = new int[]{j, i};
