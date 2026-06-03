47bb343edd28987e4cd37fdc2b23c4e529633ee0
Pierre <e81568u@etu.univ-lorraine.fr>
2026-06-03 11:25:33 +0200

    update : deplacement Ghost

--- projet_zeldiablo/src/zeldiablo/entite/Ghost.java
+++ projet_zeldiablo/src/zeldiablo/entite/Ghost.java
@@ -7,11 +7,12 @@ import zeldiablo.exception.ActionInconnueException;
 
 public class Ghost extends Personnage {
     // ########## Constructeurs ##########
+
     /**
      * Cree un nouveau monstre.
      *
-     * @param x la coordonnee x
-     * @param y la coordonnee y
+     * @param x   la coordonnee x
+     * @param y   la coordonnee y
      * @param vie les points de vie
      */
     public Ghost(int x, int y, int vie) {
@@ -22,16 +23,7 @@ public class Ghost extends Personnage {
 
     public void deplacer(Jeu jeu, Commande commandeUser) {
         int[] coord = jeu.getSuivant(this.x, this.y, commandeUser);
-        try {
-
-            // Le Ghost peut se déplacer à travers les murs, donc on ne vérifie pas les déplacements
-            if (jeu.getChar(coord[0], coord[1]) == Labyrinthe.VIDE) {
-               this.setPos(coord[0], coord[1]);
-                setPos(coord[0], coord[1]);
-            }
-        } catch (ActionInconnueException e) {
-            // Ignorer le déplacement si c'est un mur ou un mur friable ou un monstre
-        }
-
+        setPos(coord[0], coord[1]);
     }
 }
+
